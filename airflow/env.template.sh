99bf5faeb261b0103d49a8bb729b10d0cdf53c13
Mjumbe Poe <mjumbewu@gmail.com>
2021-11-03 23:10:48 -0400

    Reduce the number of webserver workers for Airflow

--- airflow/env.template.sh
+++ airflow/env.template.sh
@@ -8,7 +8,7 @@ export AIRFLOW__CORE__SQL_ALCHEMY_CONN=postgresql+psycopg2://postgres:${AIRFLOW_
 export AIRFLOW__CORE__FERNET_KEY=''
 export AIRFLOW__CORE__DAGS_ARE_PAUSED_AT_CREATION='true'
 export AIRFLOW__CORE__LOAD_EXAMPLES='false'
-export AIRFLOW__WEBSERVER__WORKERS=3
+export AIRFLOW__WEBSERVER__WORKERS=2
 export AIRFLOW__API__AUTH_BACKEND='airflow.api.auth.backend.basic_auth'
 
 # Pipeline script vars
